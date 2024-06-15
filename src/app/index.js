043d3a70ee577d8e4e8d0fcb1c3a6ed84ce1e2f3
Wilksonflor <wilksonflor12@gmail.com>
2024-06-14 21:33:29 -0300

    feat: inserido uma pequena lógica para testar o login

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,85 @@
-import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { Image } from "expo-image";
-// import CustomButton from "../components/CustomButton";
 import { Link, router } from "expo-router";
 import { useForm } from "react-hook-form";
 import CustomText from "../components/CustomText";
+import ErrorMessageComponent from "../components/ErrorMessageComponent";
+// import { Home } from "../pages/Home/Home";
 
 export default function Login() {
   const { control, handleSubmit } = useForm();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ email: "", password: "" });
 
-  console.log(email, password);
+  // Simular usuário do backend
+  const validUsers = [
+    {
+      email: "wilkson@gmail.com",
+      password: "senha1",
+    },
+    { email: "wilkson2@gmail.com", password: "senha2" },
+  ];
+
   const handleLogin = () => {
-    // lógica do login
-    console.log("entrou");
+    setError({ email: "", password: "" });
+
+    if (!isValidEmail(email)) {
+      setError((prevError) => ({
+        ...prevError,
+        email: "Por favor, insira um email válido.",
+      }));
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      setError((prevError) => ({
+        ...prevError,
+        password: "Por favor, insira sua senha.",
+      }));
+      return;
+    }
+
+    const user = validUsers.find((user) => user.email === email);
+
+    if (!user) {
+      setError((prevError) => ({
+        ...prevError,
+        email: "Este email não está registrado.",
+      }));
+      return;
+    }
+
+    if (user.password !== password) {
+      setError((prevError) => ({ ...prevError, password: "Senha incorreta." }));
+      return;
+    }
+
+    setIsLoading(true);
+    console.log("Realizando login...");
+    setTimeout(() => {
+      setIsLoading(false);
+      console.log("Login bem-sucedido para:", email);
+      // Navegar para próxima tela que é a principal após o login
+      navigation.navigate("Home");
+    }, 2000);
+  };
+
+  const isValidEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const isValidPassword = (password) => {
+    return password.length > 0;
   };
 
   return (
@@ -27,37 +91,47 @@ export default function Login() {
 
       <View>
         <TextInput
-          control={control}
           style={styles.input}
           placeholder="E-mail"
           value={email}
           onChangeText={(text) => setEmail(text)}
         />
+        {error.email && (
+          <ErrorMessageComponent>{error.email}</ErrorMessageComponent>
+        )}
         <TextInput
-          control={control}
           style={styles.input}
           placeholder="Senha"
           secureTextEntry
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
+        {error.password && (
+          <ErrorMessageComponent>{error.password}</ErrorMessageComponent>
+        )}
       </View>
 
       <View style={styles.lembrarSenha}>
-        <Link style={styles.lembrarSenhaText} href="/recuperar">
-          Esqueceu a senha?
+        <Link href="/recuperar">
+          <Text style={styles.lembrarSenhaText}>Esqueceu a senha?</Text>
         </Link>
       </View>
 
-      <Pressable onPress={handleSubmit(handleLogin)} style={styles.btnEntrar}>
-        <Text style={styles.textEntrar}>Entrar</Text>
+      <Pressable style={styles.btnEntrar} onPress={handleLogin}>
+        {isLoading ? (
+          <ActivityIndicator size="small" color="#fff" />
+        ) : (
+          <Text style={styles.textEntrar}>Entrar</Text>
+        )}
       </Pressable>
 
       <View style={styles.containerFooter}>
         <CustomText style={{ fontSize: 20, color: "gray" }}>
           Não tem cadastro?{" "}
-          <Link style={{ fontWeight: "bold", color: "black" }} href="/signup">
-            Se cadastre agora!
+          <Link href="/signup">
+            <Text style={{ fontWeight: "bold", color: "black" }}>
+              Se cadastre agora!
+            </Text>
           </Link>
         </CustomText>
       </View>
@@ -90,9 +164,7 @@ const styles = StyleSheet.create({
     width: "100%",
     flexDirection: "row",
     justifyContent: "flex-end",
-
     paddingHorizontal: 26,
-
     marginTop: 19,
     marginBottom: 41,
   },
@@ -102,7 +174,6 @@ const styles = StyleSheet.create({
     color: "#757575",
     fontWeight: 400,
   },
-
   btnEntrar: {
     width: 215,
     height: 50,
@@ -115,6 +186,7 @@ const styles = StyleSheet.create({
   textEntrar: {
     fontWeight: "bold",
     fontSize: 18,
+    color: "#fff",
   },
   containerFooter: {
     marginTop: 20,
