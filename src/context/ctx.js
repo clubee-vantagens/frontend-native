ffc05767be8ee95df6d6810e178cd39dfa093219
Bernardo <vargasbernardo97@gmail.com>
2024-07-08 16:45:20 -0300

    refact: codigo alterado para receber refreshtoken, e checar a validade do accesstoken

--- src/context/ctx.js
+++ src/context/ctx.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStorageState } from '../hooks/useAsyncState';
 import axios from 'axios';
 
@@ -8,6 +8,9 @@ const AuthContext = React.createContext({
   signOut: async () => null,
   session: null,
   isLoading: false,
+  error: null,
+  setError: () => {},
+  refreshAccessToken: async () => null
 });
 
 // Custom hook to use the AuthContext
@@ -24,7 +27,44 @@ export function useSession() {
 // SessionProvider component to provide authentication context
 export function SessionProvider(props) {
   const [[isLoading, session], setSession] = useStorageState('session');
+  const [refreshToken, setRefreshToken] = useStorageState('refreshToken')
   const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const checkTokenExpiration = async () => {
+      if (session && refreshToken) {
+        const {exp} = JSON.parse(atob(session.split('.')[1]))
+        const expirationTime = exp * 1000
+        console.log(expirationTime)
+        const currentTime = new Date().getTime()
+        console.log(currentTime);
+        if (expirationTime - currentTime < 5 * 60 * 1000) {
+          console.log(true)
+          await refreshAccessToken()
+        }
+      }
+    }
+    const intervalId = setInterval(checkTokenExpiration, 60 * 1000)
+    return () => clearInterval(intervalId)
+  }, [session, refreshToken])
+
+  const refreshAccessToken = async () => {
+    try {
+      console.log('token:',session);
+      console.log('refresh:', refreshToken[1]);
+      const response = await axios.post('http://localhost:8080/api/users/newtoken', {
+        expiredAccessToken: session, 
+        refreshToken: refreshToken[1]
+      })
+      setSession(response.data.newAccessToken)
+      setRefreshToken(response.data.newRefreshToken)
+      setError(null)
+    } catch (error) {
+      console.log(error);
+      setError(error.response?.data)
+      signOut()
+    }
+  }
 
   const signIn = async (email, password) => {
     try {
@@ -32,24 +72,28 @@ export function SessionProvider(props) {
         email,
         password,
       });
-      setSession(response.data.tokenSession);
+      setSession(response.data.accessToken);
+      setRefreshToken(response?.data.refreshToken)
       setError(null)
     } catch (error) {
       setError(error.response.data || "Erro inesperado, tente novamente.")
     }
   };
+ const signOut = () => {
+    setSession(null)
+    setRefreshToken(null);
+  }
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        signOut: () => {
-          setSession(null);
-        },
+        signOut,
         session,
         isLoading,
         error,
-        setError
+        setError,
+        refreshAccessToken
       }}
     >
       {props.children}
