70c0b3b6cf55b23d1c4dc7d5929fccc8cd7e5ab8
Bernardo <vargasbernardo97@gmail.com>
2024-10-13 11:09:09 -0300

    refact: usando constantes do expo para pegar o ip dinamicamente de quem estiver no projet

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,10 @@
+import Constants from 'expo-constants';
+
+const ip = Constants.manifest2.extra.expoGo.debuggerHost.split(':').shift()
+
+
+
+
+
 // export const api_url = 'http://localhost:8080/api'
-export const api_url = 'http://192.168.0.50:8080/api'
+export const api_url = `http://${ip}:8080/api`
