92b00f4a38694c7768651ba4ed50a2866d2c1f04
Wilkson Flor <wilksonflor12@gmail.com>
2025-01-17 08:57:44 -0300

    Feat: Avaliaçõe e  resgates

---
+++ src/components/UserData/avaliations.js
@@ -0,0 +1,23 @@
+export const MyAvaliations = [
+  {
+    title: "Livraria Central",
+    date: "23/09/2024",
+    avaliation: 4.5,
+    text: "Ótima loja com uma grande variedade de livros.",
+    isFavorite: true, // Indica que é favorita
+  },
+  {
+    title: "Loja de Música XYZ",
+    date: "23/09/2024",
+    avaliation: 3.8,
+    text: "Boa experiência, mas a entrega demorou.",
+    isFavorite: false, // Não é favorita
+  },
+  {
+    title: "Padaria da Manu",
+    date: "13/08/2024",
+    avaliation: 5.0,
+    text: "A padaria é um verdadeiro achado! Atendimento cordial, ambiente acolhedor e uma variedade incrível de produtos fresquinhos.",
+    isFavorite: true,
+  },
+];
