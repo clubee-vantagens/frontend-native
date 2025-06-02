3e5a39cd81c40bc1dc41d70af3ea0fcda41d9783
NicolasMO <nicolas_oliveira97@hotmail.com>
2025-06-01 21:47:25 -0300

    inclusão das setas de navegacao na tela de Onboarding

--- src/app/view/screen/authentication/OnBoardingScreen.js
+++ src/app/view/screen/authentication/OnBoardingScreen.js
@@ -5,11 +5,15 @@ import CustomText from "../../../../components/CustomText";
 import { scale, verticalScale } from "react-native-size-matters";
 import { router } from 'expo-router';
 import { useSession } from "../../../../context/ctx";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const { width } = Dimensions.get('window');
 
 const OnBoardingScreen = () => {
+  // Controla a coloração da progressBar
   const [currentStep, setCurrentStep] = useState(0);
+  // Controla a visibilidade das setas enquanto rola
+  const [showArrows, setShowArrows] = useState(true);
   const flatListRef = useRef();
   const { session } = useSession();
 
@@ -51,6 +55,17 @@ const OnBoardingScreen = () => {
     router.replace('/signup')
   }
 
+  // Quando começa a rolar, as setas somem
+  const handleScrollBegin = () => {
+    setShowArrows(false);  
+  };
+
+  // Quando a rolagem termina, as setas aparecem novamente
+  const handleScrollEnd = () => {
+    setShowArrows(true);  
+  };
+
+
   // renderItem transforma cada "Tela do onboarding (3 no momento) em um item"
   // Definido em pages seu titulo e imagem
   // Renderizando o resto dos componentes e mostrando-os em FlatList
@@ -59,9 +74,32 @@ const OnBoardingScreen = () => {
       <CustomText style={styles.upperText}>{item.title}</CustomText>
       <Image source={item.image} style={styles.image} />
 
-      <Pressable style={styles.signInButton} onPress={handleCadastrar}>
-        <CustomText style={styles.signInText}>Cadastre-se</CustomText>
-      </Pressable>
+       {/* Setas de navegação */}
+       {showArrows && (
+          <View style={styles.arrowsContainer}>
+            {/* Seta Esquerda */}
+            {currentStep > 0 && (
+              <AntDesign
+                name="left"
+                size={24}
+                color="black"
+                onPress={() => flatListRef.current.scrollToIndex({ index: currentStep - 1 })}
+                style={styles.arrowLeft}
+              />
+            )}
+
+            {/* Seta Direita */}
+            {currentStep < pages.length - 1 && (
+              <AntDesign
+                name="right"
+                size={24}
+                color="black"
+                onPress={() => flatListRef.current.scrollToIndex({ index: currentStep + 1 })}
+                style={styles.arrowRight}
+              />
+            )}
+          </View>
+        )}
     </View>
   );
 
@@ -80,7 +118,13 @@ const OnBoardingScreen = () => {
         showsHorizontalScrollIndicator={false}
         onScroll={handleScroll}
         ref={flatListRef}
+        onScrollBeginDrag={handleScrollBegin}  // Detecta quando começa a rolar
+        onScrollEndDrag={handleScrollEnd}      // Detecta quando termina de rolar
       />
+
+      <Pressable style={styles.signInButton} onPress={handleCadastrar}>
+        <CustomText style={styles.signInText}>Cadastre-se</CustomText>
+      </Pressable>
 
       <Pressable onPress={handlePular} style={styles.pularButton}>
         <CustomText style={styles.pularText}>Pular</CustomText>
@@ -110,22 +154,44 @@ const styles = StyleSheet.create({
     width: scale(300),
     height: verticalScale(325),
     resizeMode: "contain",
-    marginTop: verticalScale(30),
+    marginTop: verticalScale(20),
     marginBottom: verticalScale(10),
   },
   upperText: {
     fontSize: 20,
     textAlign: 'left',
     fontWeight: 'bold',
-    marginTop: -50,
+    marginTop: -100,
+  },
+  arrowsContainer: {
+    position: 'absolute',
+    top: '50%',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: scale(20),
+    zIndex: 10,
+  },
+  arrowLeft: {
+    position: 'absolute',
+    left: 5,
+    transform: [{ translateY: -12 }],
+  },
+  arrowRight: {
+    position: 'absolute',
+    right: 5,
+    transform: [{ translateY: -12 }],
   },
   signInButton: {
-    marginTop: 20,
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 120,
     borderWidth: 1,
     borderColor: "rgba(21, 15, 2, 1)",
     borderRadius: 30,
     width: 315,
     paddingVertical: 10,
+    zIndex: 10,
   },
   signInText: {
     fontSize: 18,
@@ -136,6 +202,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 50,
     left: 40,
+    zIndex: 10,
   },
   pularText: {
     color: '#000',
